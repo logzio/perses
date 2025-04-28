cfb5f0266148eec492da77c3f20ac9b0f7fa8a91
gavriel-m <MGavrielG@Gmail.com>
2025-04-28 14:16:03 +0300

    Format fix
    Signed-off-by: gavriel-m <MGavrielG@Gmail.com>

--- ui/dashboards/src/components/Variables/VariableEditor.tsx
+++ ui/dashboards/src/components/Variables/VariableEditor.tsx
@@ -103,7 +103,7 @@ export function VariableEditor(props: {
         description:
           // LOGZ.IO CHANGE START:: Micro copy changes [APPZ-260]
           'You have unsaved changes in this panel. Are you sure you want to discard them? This action can’t be undone.',
-          // LOGZ.IO CHANGE END:: Micro copy changes [APPZ-260]
+        // LOGZ.IO CHANGE END:: Micro copy changes [APPZ-260]
       });
     } else {
       props.onCancel();
