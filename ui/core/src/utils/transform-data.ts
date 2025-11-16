e23b897727cd91bc0777b5edcce2fb8af2ba55b3
OrReuben <orreuven1243@gmail.com>
2025-11-16 19:00:52 +0200

    Fix types
    Signed-off-by: OrReuben <orreuven1243@gmail.com>

--- ui/core/src/utils/transform-data.ts
+++ ui/core/src/utils/transform-data.ts
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 import { Transform } from '@perses-dev/core';
+import { useMemo } from 'react';
 
 /*
  * Join: Regroup rows with equal cell value in a column.
@@ -252,4 +253,11 @@ export function transformData(
       }, {});
   });
   return result;
+}
+
+export function useTransformData(
+  data: Array<Record<string, unknown>>,
+  transforms: Transform[]
+): Array<Record<string, unknown>> {
+  return useMemo(() => transformData(data, transforms), [data, transforms]);
 }
