8b55a141999c675317562cf5c4c48cd0606ef4b5
OrReuben <orreuven1243@gmail.com>
2025-11-26 16:02:31 +0200

    Up
    Signed-off-by: OrReuben <orreuven1243@gmail.com>

--- ui/dashboards/src/utils/panelUtils.ts
+++ ui/dashboards/src/utils/panelUtils.ts
@@ -54,6 +54,16 @@ function getPanelBounds({ x, y, w, h }: PanelGroupItemLayout): PanelGroupItemBou
   };
 }
 
+export const randomUUID = (): ReturnType<typeof crypto.randomUUID> => {
+  return crypto?.randomUUID
+    ? crypto.randomUUID()
+    : ('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        const r = (Math.random() * 16) | 0;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }) as ReturnType<typeof crypto.randomUUID>);
+};
+
 export type UnpositionedPanelGroupItemLayout = Omit<PanelGroupItemLayout, 'x' | 'y'>;
 
 /**
