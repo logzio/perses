ce4aec714f0843b0ea40e1c0edbee8b109fcc523
Gavriel Mor <98694153+Gavriel-M@users.noreply.github.com>
2026-02-08 13:55:45 +0200

    Download Button Tooltip (#58)
    Signed-off-by: gavriel-m <MGavrielG@Gmail.com>

--- ui/dashboards/src/constants/user-interface-text.ts
+++ ui/dashboards/src/constants/user-interface-text.ts
@@ -18,6 +18,7 @@ export const TOOLTIP_TEXT = {
   editDatasources: 'Edit datasources',
   editJson: 'Edit JSON',
   editVariables: 'Edit variables',
+  download: 'Download',
   viewJson: 'View JSON',
   // Group buttons
   addPanelToGroup: 'Add panel to group',
