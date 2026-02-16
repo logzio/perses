fa936ac00444ea0544fc862ae2519f451af7dfd2
OrReuben <orreuven1243@gmail.com>
2026-02-16 16:38:29 +0200

    Up
    Signed-off-by: OrReuben <orreuven1243@gmail.com>

--- ui/dashboards/src/components/Panel/PanelContent.tsx
+++ ui/dashboards/src/components/Panel/PanelContent.tsx
@@ -30,7 +30,6 @@ export interface PanelContentProps extends Omit<PanelProps<UnknownSpec>, 'queryR
  */
 export function PanelContent(props: PanelContentProps): ReactElement {
   const { panelPluginKind, definition, queryResults, spec, contentDimensions } = props;
-  console.log('🚀 ~ PanelContent ~ queryResults:', queryResults);
   const { data: plugin, isLoading: isPanelLoading } = usePlugin('Panel', panelPluginKind, { throwOnError: true }); // LOGZIO CHANGE: `useErrorBoundary` was changed to `throwOnError` for tanstack query v4 -> v5 support
 
   // Render the panel if any query has data, or the panel doesn't have a query attached (for example MarkdownPanel).
